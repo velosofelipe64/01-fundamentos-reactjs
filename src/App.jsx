5dc968b6d570c777aee5430819a78c79f1465745
velosofelipe64 <renauxfelipe64@gmail.com>
2024-11-17 22:53:54 -0300

    Init project using Vite and create components

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import { Post } from './Post.jsx'
+
+
+export function App() {
+  return (
+    <div>
+      <Post 
+        author = "Felipe Veloso"
+        content = "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Expedita ea modi iure nihil hic aspernatur quia quisquam iusto! Quibusdam vel earum dicta iusto sunt suscipit facere dolor, fuga nulla. Reiciendis!"
+      />
+      <Post
+        author = "Gustavo Rafael"
+        content = "Um novo post"
+      />
+    </div>
+  )
+}
+
+
