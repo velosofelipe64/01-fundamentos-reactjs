d34af3cae605339c04e1b1a850cbd7f91cfea3b6
velosofelipe64 <renauxfelipe64@gmail.com>
2024-11-18 00:02:35 -0300

    Create Header componente and style

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import { useState } from 'react'
 import { Post } from './Post.jsx'
-
+import { Header } from './components/Header.jsx'
 
 export function App() {
   return (
     <div>
+      <Header></Header>
       <Post 
         author = "Felipe Veloso"
         content = "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Expedita ea modi iure nihil hic aspernatur quia quisquam iusto! Quibusdam vel earum dicta iusto sunt suscipit facere dolor, fuga nulla. Reiciendis!"
