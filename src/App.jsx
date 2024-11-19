4ca07e8f7f5164f4cd1d527ba0d1808604101f5f
velosofelipe64 <renauxfelipe64@gmail.com>
2024-11-19 00:39:48 -0300

    Create and style sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,27 @@
 import { useState } from 'react'
 import { Post } from './Post.jsx'
 import { Header } from './components/Header.jsx'
+import { Sidebar } from './components/Sidebar.jsx'
+import styles from './App.module.css'
 import './global.css'
 
 export function App() {
   return (
     <div>
       <Header/>
-      <Post 
-        author = "Felipe Veloso"
-        content = "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Expedita ea modi iure nihil hic aspernatur quia quisquam iusto! Quibusdam vel earum dicta iusto sunt suscipit facere dolor, fuga nulla. Reiciendis!"
-      />
-      <Post
-        author = "Gustavo Rafael"
-        content = "Um novo post"
-      />
+      <div className={styles.wrapper}>
+        <Sidebar/>
+        <main>
+          <Post 
+            author = "Felipe Veloso"
+            content = "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Expedita ea modi iure nihil hic aspernatur quia quisquam iusto! Quibusdam vel earum dicta iusto sunt suscipit facere dolor, fuga nulla. Reiciendis!"
+          />
+          <Post
+            author = "Gustavo Rafael"
+            content = "Um novo post"
+          />
+        </main>
+      </div>
     </div>
   )
 }
---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,21 @@
+import styles from './Sidebar.module.css';
+
+export function Sidebar() {
+    return (
+        <aside className={styles.sidebar}>
+            <img 
+                className={styles.cover} 
+                src="https://images.unsplash.com/photo-1731410612759-d93cede4edbc?q=50&w=500&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" 
+            />
+
+            <div className={styles.profile}>
+                <strong>Felipe Veloso</strong>
+                <span>Web Developer</span>
+            </div>
+
+            <footer>
+                <a href="#">Editar Seu Perfil</a>
+            </footer>
+        </aside>
+    );
+}
