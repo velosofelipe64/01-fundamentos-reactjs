229d8c1127eb1f01d41cae619cbfff87327a7519
velosofelipe64 <renauxfelipe64@gmail.com>
2024-11-22 00:30:42 -0300

    Create post component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
-import { Post } from './Post.jsx'
-import { Header } from './components/Header.jsx'
-import { Sidebar } from './components/Sidebar.jsx'
+import { Post } from './components/Post'
+import { Header } from './components/Header'
+import { Sidebar } from './components/Sidebar'
 import styles from './App.module.css'
 import './global.css'
 
---
+++ src/components/Post.jsx
@@ -0,0 +1,31 @@
+import styles from './Post.module.css';
+
+export function Post() {
+    
+    return (
+        <article className={styles.post}>
+            <header>
+                <div className={styles.author}>
+                    <img 
+                        className={styles.avatar}
+                        src="https://github.com/velosofelipe64.png"     
+                    />
+                    <div className={styles.authorInfo}>
+                        <strong>Felipe Veloso</strong>
+                        <span>Web Developer</span>
+                    </div>
+                </div>
+
+                <time title='21 de Novembro de 2024 ás 22:48h' dateTime="2024-11-21 22:48:30">Publicado há 1h</time>
+            </header>
+
+            <div className={styles.content}>
+                <p>Fala galeraa 👋</p>
+                <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀</p>
+                <p>👉<a href="">jane.design/doctorcare</a></p>
+                <p><a href="">#novoprojeto #nlw #rocketseat</a></p>
+                
+            </div>
+        </article>
+    );
+}
