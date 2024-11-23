6a9c97977d9bd61ec35ebbdf4f2a1cc14aca6a9a
Felipe Veloso <renauxfelipe64@gmail.com>
2024-11-22 22:37:55 -0300

    create comment form

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -28,8 +28,15 @@ export function Post() {
                     <a href="">#nlw</a>{' '}
                     <a href="">#rocketseat</a>
                 </p>
-                
             </div>
+
+            <form className={styles.commentForm}> 
+                <strong>Deixe seu feedback</strong>
+                <textarea
+                    placeholder='Deixe um comentário'
+                />
+                <button type="submit">Comentar</button>
+            </form>
         </article>
     );
 }
